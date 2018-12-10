0293b6cc733d9f1e914f3abbf5e6aed65dc204fc
Nisarg Shah <41580974+nisargmr@users.noreply.github.com>
2018-12-10 13:29:43 -0500

    Update 20181129103819_lit_add_localization_key_locale_unique_index_to_localizations.rb

--- db/migrate/20181129103819_lit_add_localization_key_locale_unique_index_to_localizations.rb
+++ db/migrate/20181129103819_lit_add_localization_key_locale_unique_index_to_localizations.rb
@@ -2,8 +2,5 @@ class LitAddLocalizationKeyLocaleUniqueIndexToLocalizations < Rails::VERSION::MA
                                                               ActiveRecord::Migration[4.2] :
                                                               ActiveRecord::Migration
   def change
-    unless index_exists?(:lit_localizations, [:localization_key_id, :locale_id])
-      add_index :lit_localizations, [:localization_key_id, :locale_id], unique: true
-    end
   end
 end
